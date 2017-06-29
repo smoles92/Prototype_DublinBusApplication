a5d86773bad236ba7e677e75b598cd5ed58e9913
minogud2 <darragh.minogue@ucdconnect.ie>
2017-06-29 16:03:23 +0100

    Added the jupyter notebook

--- DublinBusAPIs/DublinBusAPI.py
+++ DublinBusAPIs/DublinBusAPI.py
@@ -1,15 +1,17 @@
 import requests
+import json
+import pandas as pd
 
 # Retrieve Real Time Bus Information (for bus stop 1304 (which is my stop))
 # See section 3.4.1 of the document
-"""base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=1304&routeid&maxresults&operator&format=json'
+base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=807&routeid=46a&maxresults&operator&format=json'
 response = requests.get(base_url)
 results = response.json()
 counter = 1
 for i in results['results']:
     print('Result Nº' + str(counter) + ':')
     print(i)
-    counter += 1"""
+    counter += 1
 
 # Retrieve Timetable Bus Information by Date
 # See section 3.4.2 of the document
@@ -23,30 +25,33 @@ for i in results['results']:
 
 # Retrieve Full Timetable Bus Information
 # See section 3.4.3 of the document
-"""base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/timetableinformation?type=week&stopid=1304&routeid=17&format=json'
+"""base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/timetableinformation?operator=bac&type=week&stopid=1304&routeid=17&format=json'
 response = requests.get(base_url)
 results = response.json()
 for i in results['results']:
-    print(i)"""
-
+    print(i)
+"""
 # Retrieve Bus Stop Information
 # See section 3.4.4 of the document
 # Leave stopid and stopname empty to get info on all stops. Fill in to get just that stop.
-"""base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/busstopinformation?stopid=1304&format=json'
+"""base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/busstopinformation?operator=bac&format=json'
 response = requests.get(base_url)
-results = response.json()
-for i in results['results']:
-    print(i)"""
+data = response.json()
+data2 = data['results'][8]
+# data3 = data2['operators']
+# df = pd.DataFrame(data2)
+print(data2)
+df.to_csv('results.csv')"""
 
 # Retrieve Route Information
 # See section 3.4.5 of the document
 # Operator bac seems to equate to Dublin Bus.
-base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/routeinformation?routeid=46A&operator=bac&format=json'
+"""base_url = 'https://data.dublinked.ie/cgi-bin/rtpi/routeinformation?routeid=46A&operator=bac&format=json'
 response = requests.get(base_url)
 results = response.json()
 stops = results['results'][0]
 for i in stops['stops']:
-    print(i)
+    print(i)"""
 
 # Retrieve Operator Informator
 # See section 3.4.6 of the document
