0f6c2901f945afa7a9b5c31194a0da613fe451ba
LukeO1 <londerwater1@gmail.com>
2017-07-04 19:24:31 +0100

    updated bus finder script to also calculate delay

--- DublinBusAPIs/bus_location_finder.py
+++ DublinBusAPIs/bus_location_finder.py
@@ -69,6 +69,27 @@ def get_due_time(stop_id, route_id):
     return first_3_buses
 
 
+# Receives the bus list and finds the delay for each of them
+def get_delay(list_of_buses):
+    """Receives the 3 buses arriving at the source, and appends their respective delay to each of their dictionaries"""
+    # Iterates over all the buses in the list and gets the delay for each bus
+    for i in list_of_buses:
+        # Calculate the delay, by first spliting the string (HH:MM:SS), into hours, minutes and seconds, then converting it to
+        # ints and calculating the seconds. Does it for both the scheduled arrival time and the actual predicted arrival time
+        scheduled_hours, scheduled_minutes, scheduled_seconds = i['scheduledarrivaldatetime'][11:].split(':')
+        scheduled_arrival_seconds = int(scheduled_hours) * 3600 + int(scheduled_minutes) * 60 + int(scheduled_seconds)
+        actual_hours, actual_minutes, actual_seconds = i['arrivaldatetime'][11:].split(':')
+        actual_arrival_seconds = int(actual_hours) * 3600 + int(actual_minutes) * 60 + int(actual_seconds)
+
+        # Left this print here just in case we want to visually compare the times
+        # print(i['scheduledarrivaldatetime'], i['arrivaldatetime'])
+
+        # Calculate the delay
+        delay = scheduled_arrival_seconds - actual_arrival_seconds
+
+        # Append the delay to the dictionary that represents each bus, with key 'delay'
+        i['delay'] = delay
+
 # Receives the 3 bus list, and filters it to just the first one. Useful various functions.
 def filter_buses(list_of_buses):
     """Receives bus list and returns only the first bus on that list."""
@@ -121,7 +142,7 @@ def get_closest_bus_stop(due_time, stop_src, stop_ids, route_id):
 
             # If we found a 'Due' or within the arrival_within_minutes, return that index. That is the index of the stop where our bus is at/close to.
             if possible_stop['duetime'] == 'Due' or int(possible_stop['duetime']) <= arrival_within_minutes:
-                print('Found the bus with', due_time, 'due time')
+                print('Found the bus with', due_time, 'minutes due time.')
                 return pointer
             else:
                 # If the due time at the possible stop is less than the one at SRC, we're on the right path, and need to look for a stop farther from the SRC
@@ -173,17 +194,24 @@ def main():
     # Get due times for first 3 buses at chosen origin, for specified route
     first_3_buses = get_due_time(at_stop_id, bus_route)
 
+    # Get the delays for each of the buses
+    get_delay(first_3_buses)
+
     # Find where the buses are
     for i in first_3_buses:
         # If the bus is due at the SRC stop, no need to look for it
-        print('Original due time is', i['duetime'])
+        print('Original due time is', i['duetime'], 'minutes.')
         if i['duetime'] == 'Due' or int(i['duetime']) <= 2:
-            print('The bus it at', at_stop_id)
+            print('The bus it at', str(at_stop_id) + '.')
+            print('Bus is delayed by', i['delay'], 'seconds. Or approximately', round(i['delay'] / 60), 'minutes.')
+            print('\n<<---------------------------------------------------------->>')
             continue
 
         # But if its not, we will need to go into the bus finder algorithm
         pointer = get_closest_bus_stop(i['duetime'], at_stop_id, stop_ids, bus_route)
-        print('The bus is at', stop_ids[pointer])
+        print('The bus is at', str(stop_ids[pointer]) + '.')
+        print('Bus is delayed by', i['delay'], 'seconds. Or approximately', round(i['delay'] / 60), 'minutes.')
+        print('\n<<---------------------------------------------------------->>')
 
         # Next we reduce the pointer (our index) by one, so we don't risk getting the same bus again (except detailed exception as explained previously)
         at_stop_id = str(stop_ids[pointer - 1])
