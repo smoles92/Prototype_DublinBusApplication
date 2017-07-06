4cfc1b680fdfba8cdb8b9fe7bac64d975553971f
Stephen <stephen.moles@ucdconnect.ie>
2017-07-06 17:51:56 +0100

    Removed sample Django website, replaced with current DublinBus homepage

---
+++ Django - Practice Website/dublinbusjourney/dublinbuspredict/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DublinbuspredictConfig(AppConfig):
+    name = 'dublinbuspredict'
--- Django - Practice Website/website/music/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Album, Song
-
-admin.site.register(Album) # Register album
-admin.site.register(Song) # Register song
